e833d2eea273456b6db0ed9749159d10f36e76fe
Levochka108 <d.klochkov@gmail.com>
2023-06-18 13:07:00 +0300

    test

--- Note.py
+++ Note.py
@@ -1,5 +1,5 @@
 class Note:
-    def __int__(self, id, title, body, timestamp):
+    def __init__(self, id, title, body, timestamp):
         self.id = id
         self.title = title
         self.body = body
--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import datetime
+from NoteManager import NoteManager
+from NoteRepository import NoteRepository
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main_menu():
+    note_repository = NoteRepository("notes.json")
+    note_manager = NoteManager(note_repository)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        print("=== Меню ===")
+        print("1. Вывести все заметки")
+        print("2. Добавить заметку")
+        print("3. Редактировать заметку")
+        print("4. Удалить заметку")
+        print("5. Фильтровать заметки по дате")
+        print("0. Выход")
+        choice = input("Выберите действие: ")
+
+        if choice == "1":
+            note_manager.list_notes()
+        elif choice == "2":
+            title = input("Введите заголовок заметки: ")
+            body = input("Введите текст заметки: ")
+            note_manager.add_note(title, body)
+        elif choice == "3":
+            note_id = input("Введите ID заметки для редактирования: ")
+            new_title = input("Введите новый заголовок (оставьте пустым, чтобы не изменять): ")
+            new_body = input("Введите новый текст (оставьте пустым, чтобы не изменять): ")
+            note_manager.edit_note(note_id, new_title, new_body)
+        elif choice == "4":
+            note_id = input("Введите ID заметки для удаления: ")
+            note_manager.delete_note(note_id)
+        elif choice == "5":
+            start_date = input("Введите начальную дату в формате ГГГГ-ММ-ДД: ")
+            end_date = input("Введите конечную дату в формате ГГГГ-ММ-ДД: ")
+            note_manager.filter_notes_by_date(start_date, end_date)
+        elif choice == "0":
+            break
+        else:
+            print("Некорректный выбор.")
+
+
+if __name__ == "__main__":
+    main_menu()
